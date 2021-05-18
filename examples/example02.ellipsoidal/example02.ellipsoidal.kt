8f8051914108b27df5c350f7ca05236a808c1589
Gang Liu <gl.cell@outlook.com>
2021-05-17 20:14:46 -0400

    Update example02.ellipsoidal.kt

--- examples/example02.ellipsoidal/example02.ellipsoidal.kt
+++ examples/example02.ellipsoidal/example02.ellipsoidal.kt
@@ -95,7 +95,7 @@ fun main() {
                      + "," + incidentangle.toString() + "," + reflectangle.toString() + ","
                      + csts.PickTypicalColor(i) + "\n");
        } catch(e: Exception) {
-         println("An error occurred when working on the file the BIG FILEs, then stopped.");
+         println("An error occurred when working on the files iterated.alldata.*.csv, then stopped.");
          e.printStackTrace()
          exitProcess(4);
        }
